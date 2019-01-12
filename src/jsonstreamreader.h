5742ea75bbddc766678754d992835f52750da22f
papparazzo <pappi-@gmx.de>
2019-01-13 00:28:43 +0100

    wip...

---
+++ src/jsonstreamreader.h
@@ -0,0 +1,63 @@
+/*
+ *  Project:    moba-common
+ *
+ *  Copyright (C) 2016 Stefan Paproth <pappi-@gmx.de>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+#pragma once
+
+#include <memory>
+#include <exception>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+
+class JsonStreamReaderException : public std::exception {
+
+    public:
+        virtual ~JsonStreamReaderException() throw() {
+        }
+
+        JsonStreamReaderException(const std::string &what) {
+            this->what__ = what;
+        }
+
+        virtual const char* what() const throw() {
+            return this->what__.c_str();
+        }
+
+    private:
+        std::string what__;
+
+};
+
+class JsonStreamReader;
+
+using JsonStreamReaderPtr = std::shared_ptr<JsonStreamReader>;
+
+class JsonStreamReader {
+
+    public:
+        virtual ~JsonStreamReader() {};
+        virtual char read() {
+            char data;
+            ::recv(socket, &data, sizeof(data), 0);
+            return data;
+        }
+};
+
+
